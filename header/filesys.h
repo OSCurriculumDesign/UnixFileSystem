758ecd202702af6bb69e80338c9faba67cd58239
Eric Lee <869909541@qq.com>
2018-07-04 10:58:33 +0800

    finish system.h

--- header/filesys.h
+++ header/filesys.h
@@ -65,3 +65,122 @@
 
 /*fseek origin*/
 #define SEEK_SET 0
+
+
+
+/* 文件系统数据结构 */
+struct inode{
+   struct inode *i_forw;
+   struct inode *i_back;
+   char i_flag;
+   unsigned int i_ino;
+   unsigned int i_count;
+   unsigned short di_number;
+   unsigned short di_mode;
+   unsigned short di_uid;
+   unsigned short di_gid;
+   unsigned int di_size;
+   unsigned int di_addr[NADDR];
+ };
+
+
+struct dinode{
+  unsigned short di_number;
+  unsigned short di_mode;
+  unsigned short di_uid;
+  unsigned short di_gid;
+  unsigned long di_size;
+  unsigned int di_addr[NADDR];
+};
+
+
+struct direct{
+   char d_name[DIRSIZ];
+   unsigned int d_ino;
+ };
+
+
+struct filsys{
+  unsigned short s_isize;
+  unsigned short s_fsize;
+  unsigned int s_nfree;
+  unsigned short s_pfree;
+  unsigned short s_free[NICFREE];
+
+  unsigned int s_ninode;
+  unsigned short s_pinode;
+  unsigned int s_inode[NICINOD];
+  unsigned int s_rinode;
+
+  char s_fmod;
+};
+
+
+struct pwd{
+   unsigned short p_uid;
+   unsigned short p_gid;
+   char pasword[PWOSIZ];
+ };
+
+
+ struct dir{
+   struct direct direct[DIR NUM];
+   int size;
+ };
+
+
+ struct hinode{
+   struct inode * i_forw;
+ };
+
+
+ struct file{
+   char f_flag;
+   unsigned int f_count;
+   struct inode * f_inode;
+   unsigned long f_off;
+ };
+
+
+ struct user{
+   unsigned short u_default_mode;
+   unsigned short u_uid;
+   unsigned short u_gid;
+   unsigned short u_ofile[NOFILE];
+ };
+
+
+extern struct hinode hinode[NHINO];
+extern struct dir dir;
+extern struct file sys_ofile[SYSOPENFILE];
+extern struct filsys filsys;
+extern struct pwdpwd[PWDNUM];
+extern struct useruser[USERNUM];
+extern FILE * fd ;
+extern struct inode * cur_path_inode;
+extern int user_id, file_block;
+
+
+extern struct inode* iget();
+extern iput();
+extern unsigned int baloc();
+extern bfree();
+extern struct inode *ialoc();
+extern ifree();
+extern unsigned int namei();
+extern unsigned int iname();
+extern unsigned int aces();
+extern _dir();
+extern mkdir();
+extern chdir();
+extern dirlt();
+extern unsigned short open();
+extern create();
+extern unsigned int read();
+extern unsigned write();
+extern int login();
+extern logout();
+extern instal();
+extern format();
+extern close();
+extern halt();
